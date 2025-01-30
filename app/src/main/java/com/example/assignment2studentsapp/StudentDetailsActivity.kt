208c557d997cb1bd986b568caedc3216a0d347dc
ido sharonn <idosharon001@gmail.com>
2025-01-31 00:32:08 +0200

    finished

--- app/src/main/java/com/example/assignment2studentsapp/StudentDetailsActivity.kt
+++ app/src/main/java/com/example/assignment2studentsapp/StudentDetailsActivity.kt
@@ -1,13 +1,26 @@
 package com.example.assignment2studentsapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
+
 
 class StudentDetailsActivity : AppCompatActivity() {
+    private val editStudentLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Constants.RESULT_DELETE || result.resultCode == Constants.RESULT_UPDATE) {
+                finish()
+            }
+
+}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,10 +35,31 @@ class StudentDetailsActivity : AppCompatActivity() {
         val idTextView: TextView = findViewById(R.id.student_id_text_view)
         val phoneTextView: TextView = findViewById(R.id.student_phone_text_view)
         val addressTextView: TextView = findViewById(R.id.student_address_text_view)
+        val checkBoxView: CheckBox = findViewById(R.id.list_row_check_box)
+        val editButton: Button = findViewById(R.id.student_edit_button)
 
-        nameTextView.text = intent.getStringExtra("StudentName")
-        idTextView.text = intent.getStringExtra("StudentName")
-        phoneTextView.text = intent.getStringExtra("StudentName")
-        addressTextView.text = intent.getStringExtra("StudentName")
+        nameTextView.text = intent.getStringExtra("studentName")
+        idTextView.text = intent.getStringExtra("studentId")
+        phoneTextView.text = intent.getStringExtra("studentPhone")
+        addressTextView.text = intent.getStringExtra("studentAddress")
+        val isChecked = intent.getBooleanExtra("studentChecked", false)
+        checkBoxView.isChecked = isChecked
+
+        editButton.setOnClickListener {
+            val intent = Intent(this, EditStudentActivity::class.java).apply {
+                putExtra("studentName", nameTextView.text.toString())
+                putExtra("studentId", idTextView.text.toString())
+                putExtra("studentPhone", phoneTextView.text.toString())
+                putExtra("studentAddress", addressTextView.text.toString())
+                putExtra("isChecked", checkBoxView.isChecked)
+            }
+
+            editStudentLauncher.launch(intent)
+        }
+
+        val toolbar: MaterialToolbar = findViewById(R.id.toolbar)
+
+        toolbar.setNavigationOnClickListener {
+            finish()
     }
-}
+}}
